7a1ea3f01446ea7203836721ae1f606a5de126e0
Paul Hickman <phickman@users.noreply.github.com>
2015-03-18 13:39:01 +0800

    Delete keys.h

--- keys.h
+++
@@ -1,9 +0,0 @@
-#define WLAN_SSID       ""        // cannot be longer than 32 characters!
-#define WLAN_PASS       ""
-// Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
-#define WLAN_SECURITY   WLAN_SEC_WPA2
-
-// Carriots parameters
-#define WEBSITE  "api.carriots.com"
-#define API_KEY "" // need to check if needs to be full or stream only
-#define DEVICE  "" // monitor@username.username
